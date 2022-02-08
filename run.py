b3ca7d85db6c3cb33d319ac35fcaade3ad648e6a
frankzou666 <nokiss9521@gmail.com>
2022-02-08 11:00:01 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,17 @@
+
+
+
+from myapp import app
+import logging
+
+def main():
+    """
+
+    :return:
+    """
+    logging.info('app start...')
+    app.run(host=app.config['LISTENHOST'])
+
+
+if __name__ == '__main__':
+    main()
